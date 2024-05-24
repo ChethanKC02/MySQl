ffa07fa12cca244bd46d09595361f4796e7e8648
ChethanKC02 <chethankc.369.369@gmail.com>
2024-05-24 10:01:05 +0530

    24/05/2023

--- 01 Second highest salary.sql
+++ 01 Second highest salary.sql
@@ -1,1 +1,4 @@
 select max(salary) SecondHighestSalary from Employee where salary < (select max(salary) from employee);
+
+
+select IFNULL( Null,  (select distinct Salary from Employee  order by Salary desc limit 1 offset 1) )as SecondHighestSalary
